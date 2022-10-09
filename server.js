cdef1725d6ab8e8ff59313d1672649bc788f5149
Matthews Masia <49841889+MaadMat@users.noreply.github.com>
2022-10-09 17:20:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
-const https = require("https")
 const app = express();
 const cors = require('cors')
 const path = require("path");
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
-
+const _environment  = process.env.ENVIRONMENT
 mongoose.connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}) 
+})
 
 app.use(cors({ credentials: true }, { origin: true }));
 app.use(bodyParser.json());
@@ -20,6 +19,20 @@ app.use(bodyParser.urlencoded({ limit: '5000mb', extended: true, parameterLimit:
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './public')));
 app.use('/', require('./routes/cards.route'))
-https.createServer(app).listen(PORT, () => {
+
+// ---- REDIRECT TO HTTPS ---- //
+if (_environment === 'production') {
+    app.enable('trust proxy');
+    app.use(function (req, res, next) {
+        if (req.secure) {
+            // https request, nothing to handle
+            next();
+        } else {
+            // this is an http request, redirect to https
+            res.redirect(301, 'https://' + req.headers.host + req.url);   
+        }
+    });
+}
+app.listen(PORT, () => {
     console.log(`Listening to Port: ${PORT}`)
 })
